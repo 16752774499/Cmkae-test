b8f60149df48572954728414028d43e3e0cb299e
xiaohua <2755319956@qq.com>
2023-12-29 07:08:31 -0900

    CmakeTest

---
+++ build/CMakeFiles/app2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../bin/app2"
+  "../bin/app2.pdb"
+  "CMakeFiles/app2.dir/src/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/app2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
